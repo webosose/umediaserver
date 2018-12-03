f056da23e5ef120f90d671c0bde5548ad3738c7b
ekwang.lee <ekwang.lee@lge.com>
2018-12-03 10:46:43 +0900

    Fix bug : Display connect fails when app is switched to foreground
    :Release Notes:
    Fix bug : Display connect fails when app is switched to foreground
    
    :Detailed Notes:
    There is a problem that the MDC can not connect to the video when the
    video client app repeats the background and the foreground.
    If the app enters the foreground, "TryDisplay" connect should send an
    event post to the audio / video element, but there is a problem sending
    a TryDisplay event to the audio only.
    Modified to send TryDisplay event in video element also.
    
    :Testing Performed:
    1. Run Browser
    2. Input "http://camendesign.com/code/video_for_everybody/test.html
    in URL bar
    3. Play video
    4. Open new tab
    5. Input "www.google.com"
    6. switch to first tab
    7. switch to second tab
    8. repeat 6,7 2times quickly.
    9. First tab should show video at the correct position even if repeat
    step 8
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    
    Change-Id: Id6d37cff84f961a2221231dffa11a1088c7684d0

--- src/media_display_controller/media_object.h
+++ src/media_display_controller/media_object.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2018 LG Electronics, Inc.
+// Copyright (c) 2015-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -361,7 +361,8 @@ struct states::Registered : boost::statechart::state< states::Registered, states
 struct states::VideoDisconnected : boost::statechart::state< states::VideoDisconnected,
 															 states::Registered::orthogonal<0> > {
 	typedef boost::mpl::list < boost::statechart::custom_reaction< event::TryDisplay >,
-							   boost::statechart::deferral< event::VideoConnected >
+							   boost::statechart::deferral< event::VideoConnected >,
+							   boost::statechart::transition< event::VideoConnected, states::VideoConnected >
 							 > reactions;
 	VideoDisconnected(my_context context);
 	boost::statechart::result react(const event::TryDisplay &);
